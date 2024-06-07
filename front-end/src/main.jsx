e2b10f7f0d7c0df1fd591708556e1755e39a1fb7
jayak <kumarijaya127@gmail.com>
2024-06-07 18:58:01 +0530

    add react-routers

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -2,14 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 import Layout from "./Layout.jsx";
+import Header from "./components/Header/Header.jsx";
 
-const router = createBrowserRouter();
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Header />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <App /> */}
+    <App />
     <RouterProvider router={router} />
   </React.StrictMode>
 );
